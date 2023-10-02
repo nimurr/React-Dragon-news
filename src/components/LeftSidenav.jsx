302a70727bf30bf8bdcbe35868f18a115a011180
nimurr <nimurnerob404@gmail.com>
2023-10-02 13:51:24 -0600

    added left side menuvar

--- src/components/LeftSidenav.jsx
+++ src/components/LeftSidenav.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
  
 export default function LeftSidenav() {
+
+    const [categories , setCategories] = useState([]);
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
+
+
   return (
     <div>
-       <h2>Left side navbar</h2>
+       <h2 className="text-xl font-semibold mb-5">All Caterogy</h2>
+       <div className="flex flex-col gap-1">
+            {
+                categories.map(data => <Link key={data.id} className="hover:bg-blue-100 p-2 rounded"> {data.name}</Link>)
+            }
+       </div>
     </div>
   )
 }
